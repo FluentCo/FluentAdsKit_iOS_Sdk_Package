45c09ddcdf020d4470c978986ff84495e53c3d15
Austin Wong <awong@fluentco.com>
2023-08-25 13:47:51 -0400

    update package

--- Package.swift
+++ Package.swift
@@ -17,6 +17,6 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        .binaryTarget(name: "FluentAdFlowAdsKit", url: "https://fluent-ads-kit-sdks.s3.us-east-2.amazonaws.com/ios/FluentAdFlowAdsKit.xcframework.v0.0.1.zip", checksum: "40bee60b7bda0778c06500cb182aef37889b9b533fa7ebe7aa8bae3d989d491a")
+        .binaryTarget(name: "FluentAdFlowAdsKit", url: "https://fluent-ads-kit-sdks.s3.us-east-2.amazonaws.com/ios/FluentAdFlowAdsKit.xcframework.v0.0.1.zip", checksum: "15c25b286832927bb9bfb61f25744256bc9d9d6780a2ac6db25a62a9d6ef647d")
     ]
 )
